d4e8ba6e68f3e0a81e06fefe80ec0fc326a50408
= <andresfelipe.restrepo@yuxipacific.com>
2016-06-30 20:10:36 -0500

    code finished

--- public/javascript/menuController.js
+++ public/javascript/menuController.js
@@ -30,19 +30,55 @@ var menuController = ( function () {
 		bindEventForMobileMenu();
 	};
 
+	function _listBehaviorMobile( status ) {
+		if ( status === "mobile" ) {
+			var parentsList = document.getElementsByClassName( 'parents' );
+			for ( var i = 0; i < parentsList.length; i++ ) {
+				_addGivenClass( parentsList[ i ], 'parent-mobile' );
+				_bindParentAction( parentsList[ i ] );
+			};
+
+		} else {
+			var list = document.getElementsByClassName( 'parent-mobile' );
+			while ( list.length > 0 ) {
+				list[ 0 ].classList.remove( 'parent-mobile' );
+			}
+		}
+	};
+
+	function _bindParentAction( domObj ) {
+		domObj.onclick = function () {
+			if ( _classExists( this, "open" ) ) {
+				_removeGivenClass( this, "open" )
+			} else {
+				_addGivenClass( this, "open" );
+			}
+		};
+	}
+
 	function bindEventForMobileMenu() {
 		var mobileToogle = document.getElementById( 'menu-toggle' );
+		var hugeIcon = document.getElementsByClassName( 'hugeIcon' )[ 0 ];
 		mobileToogle.onclick = function () {
-			_classExists(this, "close") ? _removeGivenClass(this, "close") : _addGivenClass(this, "close");	
+			var status = "desktop";
+			if ( _classExists( this, "close" ) ) {
+				_removeGivenClass( hugeIcon, "active" );
+				_removeGivenClass( this, "close" );
+			} else {
+				_addGivenClass( this, "close" );
+				_addGivenClass( hugeIcon, "active" );
+				status = "mobile";
+			}
 			var nav = document.getElementById( 'toolbar' );
 			nav.className === "nav" ? nav.className = "nav-mobile" : nav.className = "nav";
+			_listBehaviorMobile( status )
 		};
 	};
 
-	function _classExists(domObj, className) {
-		if (domObj.classList.length > 1) {
-			for (var i = 0; i < domObj.classList.length; i++) {
-				if(domObj.classList[i] === className)
+	function _classExists( domObj, className ) {
+		if ( domObj.classList.length > 1 ) {
+			for ( var i = 0; i < domObj.classList.length; i++ ) {
+				if ( domObj.classList[ i ] === className )
 					return true;
 			}
 			return false;
@@ -50,15 +86,15 @@ var menuController = ( function () {
 			return domObj.className === className;
 		}
 	}
-	
-	function _removeGivenClass(domObj, className){
-		domObj.classList.remove(className);
+
+	function _removeGivenClass( domObj, className ) {
+		domObj.classList.remove( className );
 	};
 
-	function _addGivenClass(domObj, className){	
-		var exists = _classExists(domObj, className);
-		if(!exists) domObj.className += " " + className;
-		
+	function _addGivenClass( domObj, className ) {
+		var exists = _classExists( domObj, className );
+		if ( !exists ) domObj.className += " " + className;
+
 	};
 
 	return {
